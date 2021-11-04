a8e1e96d9c0f205a455c2e6fe5448a9172e52007
Alex Mikhalevich <alex@mikhalevich.com>
2021-11-04 19:17:25 +0300

    Learn download tiles API

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+mod map_builder;
+
+use map_builder::get_tiles;
+
+#[tokio::main]
+async fn main() {
+    println!("Starting");
+    get_tiles(1, 1, 1, "https://a.tile.openstreetmap.org/{z}/{x}/{y}.png").await;
 }
